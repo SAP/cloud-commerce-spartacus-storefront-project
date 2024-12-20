be22660efa7cce9b1fc81e6ef54bf4ba0ab51710
kpawelczak <42094017+kpawelczak@users.noreply.github.com>
2024-12-20 14:25:10 +0100

    no bootstrap spartacus installation

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
   ],
   resolve: {
     fallback: {
-      bootstrap: path.join(__dirname, 'projects/styles/bootstrap-copy'),
+      bootstrap: path.join(__dirname, 'projects/storefrontstyles/bootstrap-copy'),
       '@spartacus/styles': path.join(__dirname, 'projects/storefrontstyles'),
       '@spartacus/user': path.join(__dirname, 'feature-libs/user'),
       '@spartacus/organization': path.join(
