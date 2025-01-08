dfb8e131ded0fb4f18d1f4bccbbf64818dac5448
Petar Markov <petarmarkov9449@gmail.com>
2025-01-08 12:21:58 +0100

    fix style issues of admin for highContrastTheme

--- projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
+++ projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
@@ -317,6 +317,8 @@ if (environment.cpq) {
         a11yPreventHorizontalScroll: true,
         a11yReorderDialog: true,
         a11yPopoverFocus: true,
+        a11yPopoverHighContrast: true,
+        a11yDropdownHighContrast: true,
         a11yScheduleReplenishment: true,
         a11yScrollToTop: true,
         a11ySavedCartsZoom: true,
